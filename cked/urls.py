397e5ac17d4ec0b1ac824270998c2b3cc1d7d99e
simon-db <freelance.simon.db@gmail.com>
2016-09-14 01:09:28 +0300

    Update urls.py

--- cked/urls.py
+++ cked/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
+from .views import elfinder, elfinder_connector
 
-
-urlpatterns =\
-    patterns('cked.views',
-             url(r'^elfinder/$', 'elfinder', name='cked_elfinder'),
-             url(r'^elfinder/connector/$', 'elfinder_connector',
-                 name='cked_elfinder_connector'),)
+urlpatterns = [
+    url(r'^elfinder/$', elfinder, name='cked_elfinder'),
+    url(r'^elfinder/connector/$', elfinder_connector, name='cked_elfinder_connector'),
+    ]

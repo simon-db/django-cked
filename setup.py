c5bb1e8ffe36d3513c56a96c437f9e91d818776d
simon-db <freelance.simon.db@gmail.com>
2016-11-01 17:01:26 +0300

    fix name

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 
 setup(
-    name='django-cked-fc',
+    name='cked',
     version='0.1.1',
     author='Future Colors (original by Svyatoslav Bulbakha)',
     author_email='info@futurecolors.ru',
